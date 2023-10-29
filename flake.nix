584e4b07981172b7c45d2dee30cafafc5449059f
Peter Tonner <peter.tonner@proton.me>
2023-10-29 14:09:52 -0400

    add xev for evaluating keeb input

--- flake.nix
+++ flake.nix
@@ -10,6 +10,6 @@
     utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
-        devShells.default = pkgs.mkShell { packages = with pkgs; [ qmk ]; };
+        devShells.default = pkgs.mkShell { packages = with pkgs; [ qmk xorg.xev ]; };
       });
 }
